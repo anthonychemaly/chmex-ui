7d440b5dab631d12dc0471261974780d08ed1d58
Anthony Chemaly <anthony@Anthonys-MacBook-Pro.local>
2023-01-02 15:41:53 +0200

    Table colots

--- src/components/Table/index.js
+++ src/components/Table/index.js
@@ -1,9 +1,20 @@
 import React from 'react'
+import { useChmexTheme } from '../../hooks'
 import styles from './styles.modules.scss'
 
-const Table = ({ children, style }) => {
+const Table = ({ children, style, borderColor, textColor }) => {
+  const { colors, dark } = useChmexTheme()
+
   return (
-    <table className={styles.table} style={style}>
+    <table
+      className={styles.table}
+      style={{
+        ...style,
+        borderColor:
+          borderColor || (dark ? colors.borderDark : colors.borderLight),
+        color: textColor || dark ? colors.white : colors.black
+      }}
+    >
       {children}
     </table>
   )
